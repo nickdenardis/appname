5b6614b555ede52c0642d5a3cb36fe7b2b009edd
Nick DeNardis <nick.denardis@gmail.com>
2013-08-15 08:43:47 -0400

    Added Sentry 2 include. Doesn't do anything yet except compile in.

--- app/config/app.php
+++ app/config/app.php
@@ -115,6 +115,7 @@ return array(
 		'Illuminate\Workbench\WorkbenchServiceProvider',
 		'Way\Generators\GeneratorsServiceProvider',
 		'Conarwelsh\MustacheL4\MustacheL4ServiceProvider',
+		'Cartalyst\Sentry\SentryServiceProvider',
 
 	),
 
@@ -180,6 +181,7 @@ return array(
 		'URL'             => 'Illuminate\Support\Facades\URL',
 		'Validator'       => 'Illuminate\Support\Facades\Validator',
 		'View'            => 'Illuminate\Support\Facades\View',
+		'Sentry'		  => 'Cartalyst\Sentry\Facades\Laravel\Sentry',
 
 	),
 
